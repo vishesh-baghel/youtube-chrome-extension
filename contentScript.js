44cf357aaf9a7c107ebbeb75bac6f1fa75c71e31
vishesh-baghel <visheshbaghel99@gmail.com>
2023-08-16 18:18:56 +0530

    added starter files

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,13 @@
 (() => {
   let youtubeLeftControls, youtubePlayer;
+  let currentVideo = "";
+
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
+
+    if (type === "NEW") {
+      currentVideo = videoId;
+      newVideoLoaded();
+    }
+  });
 })();
